19b4a9452a7f2e44156cafbbf74fcb500e51421c
Jesse Degger <degger@x-guard.nl>
2020-03-26 15:10:52 +0100

    Fix to use uuid-mongodb in the background

--- index.js
+++ index.js
@@ -1,84 +1,84 @@
-'use strict';
-
-var mongoose = require('mongoose');
-var bson = require('bson');
-var util = require('util');
-var uuidParse = require('uuid-parse');
-
-var Document = mongoose.Document;
+const mongoose = require('mongoose');
+const util = require('util');
+const MUUID = require('uuid-mongodb');
 
 function getter(binary) {
+
   if (binary == null) return undefined;
   if (!(binary instanceof mongoose.Types.Buffer.Binary)) return binary;
 
-  var len = binary.length();
-  var b = binary.read(0,len);
-  var buf = new Buffer(len);
-  var hex = '';
+  return MUUID.from(binary).toString();
 
-  for (var i = 0; i < len; i++) {
-    buf[i] = b[i];
-  }
-
-  for (var i = 0; i < len; i++) {
-    var n = buf.readUInt8(i);
-
-    if (n < 16){
-      hex += '0' + n.toString(16);
-    } else {
-      hex += n.toString(16);
-    }
-  }
-
-  return hex.substr(0, 8) + '-' + hex.substr(8, 4) + '-' + hex.substr(12, 4) + '-' + hex.substr(16, 4) + '-' + hex.substr(20, 12);
 }
 
 function SchemaUUID(path, options) {
+
   mongoose.SchemaTypes.Buffer.call(this, path, options);
 
   this.getters.push(getter);
+
 }
 
 util.inherits(SchemaUUID, mongoose.SchemaTypes.Buffer);
 
 SchemaUUID.schemaName = 'UUID';
 
-SchemaUUID.prototype.checkRequired = function(value) {
+SchemaUUID.prototype.checkRequired = function checkRequired(value) {
+
   return value instanceof mongoose.Types.Buffer.Binary;
+
 };
 
-SchemaUUID.prototype.cast = function(value, doc, init) {
-  if (value instanceof mongoose.Types.Buffer.Binary) return value;
+SchemaUUID.prototype.cast = function cast(value, doc, init) {
 
-  if (typeof value === 'string') {
-    var uuidBuffer = new mongoose.Types.Buffer(uuidParse.parse(value));
+  if (value instanceof mongoose.Types.Buffer.Binary) {
 
-    uuidBuffer.subtype(bson.Binary.SUBTYPE_UUID);
+    if (init && doc instanceof mongoose.Types.Embedded) {
 
-    return uuidBuffer.toObject();
+      return getter(value);
+    
+    }
+    return value;
+  
   }
 
-  throw new Error('Could not cast ' + value + ' to UUID.');
+  if (typeof value === 'string') {
+
+    return MUUID.from(value);
+
+  }
+
+  throw new Error(`Could not cast ${value} to UUID.`);
+
 };
 
-SchemaUUID.prototype.castForQuery = function($conditional, val) {
-  var handler;
+SchemaUUID.prototype.castForQuery = function castForQuery($conditional, val) {
+
+  let handler;
 
   if (arguments.length === 2) {
+
     handler = this.$conditionalHandlers[$conditional];
 
     if (!handler) {
-      throw new Error("Can't use " + $conditional + " with UUID.");
+
+      throw new Error(`Can't use ${$conditional} with UUID.`);
+    
     }
 
     return handler.call(this, val);
+  
   }
 
   return this.cast($conditional);
+
 };
 
-module.exports = function(mongoose) {
-  mongoose.Types.UUID = mongoose.SchemaTypes.UUID = SchemaUUID;
-}
+module.exports = function MongoosesUUID(mongooseInstance) {
 
-module.exports.UUID = SchemaUUID
+  mongoose.Types.UUID = SchemaUUID;
+  mongooseInstance.SchemaTypes.UUID = SchemaUUID;
+
+};
+
+module.exports.UUID = SchemaUUID;
